cd22a8f4e5c8c07e7fd96266aef1a6aa0b21ab1a
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2024-01-06 21:19:39 +0900

    feat: 스낵바 기능구현
    setLikeBtn에서 사소한 오류가 있었는데
    뷰모델과 액티비티가 독립적으로 진행 되기때문에 오류가 생긴것같다

--- app/src/main/java/com/example/applemarket/main/DetailActivity.kt
+++ app/src/main/java/com/example/applemarket/main/DetailActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.applemarket.Post
 import com.example.applemarket.R
 import com.example.applemarket.databinding.ActivityDetailBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DetailActivity : AppCompatActivity() {
 
@@ -57,8 +58,21 @@ class DetailActivity : AppCompatActivity() {
         setLikeBtn(post)
     }
 
+    /**
+     * 스낵바 출력 구현
+     *
+     * 맨처음 눌렀을때 좋아요 이미지가 빈하트면 스낵바가 안뜨고
+     * 하트이미지가 변경되긴하는데 다시눌러서 빈하트를 만들고 또 다시 눌러야 스낵바가 뜸
+     * 맨처음에 이미 좋아요가 눌린상태면 눌렀을때 빈하트가 되면서 바로 스낵바가 뜬다
+     * 그다음은 다시 하트 이미지가 변경되었을때 스낵바가 뜨게됨
+     *
+     * 이런 버그가 발생한 원인이 뭘까?
+     * 그건 뷰모델과 관련있는데 뷰모델과 액티비티는 서로 독립적으로 진행되기 때문에
+     * 뷰모델에선 계산하는데 스낵바로직이 바로 실행됬기 때문인걸로 추정된다
+     */
     private fun setLikeBtn(post: Post){
         binding.ivDetailLike.setOnClickListener {
+            if (!post.userlike) Snackbar.make(binding.tvDetailPrice,"관심 목록에 추가되었습니다",Snackbar.LENGTH_LONG).show()
             viewModel.onClickLike(post)
         }
     }
