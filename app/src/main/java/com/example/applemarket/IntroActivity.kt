7ade6021a24d1d09c7a1bb0c30b567922562a7aa
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2024-01-08 05:55:09 +0900

    Feat: "🚀찐 최종"
    인트로 추가

---
+++ app/src/main/java/com/example/applemarket/IntroActivity.kt
@@ -0,0 +1,31 @@
+package com.example.applemarket
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.example.applemarket.main.MainActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000) // 3초 후에 MainActivity로 전환
+    }
+}
